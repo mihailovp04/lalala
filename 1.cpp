a7231a1f689c59452f51511cef681b888065f03c
Mihailov <mihailovp04@gmail.com>
2023-09-28 11:07:47 +0300

    Initial commit

---
+++ 1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i;
+	i = 69;
+	int number = 72;
+	i = i + number;
+	int array[3] = { 0,0,0 };
+	int* iPointer = &i;
+	number = *iPointer;
+	*iPointer = 15;
+	iPointer = &array[0];
+	iPointer += 2;
+	array[0] = 5;
+	*iPointer = 6;
+	
+	return 0;
+
+}
